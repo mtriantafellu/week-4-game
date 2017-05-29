88721e4416f65ecffa7b886d8124ef5dd4f1c141
matt <mtriantafellu@yahoo.com>
2017-05-29 14:31:01 -0400

    buttons working, generating random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,126 @@
+// DISPLAY IDs:
+// randomNumber
+// totalScore
+// wins
+// losses
+
+// BUTTON IDs:
+// opalBtn
+// amberBtn
+// quartzBtn
+// zirconBtn
+// startBtn
+
+
+
 $(document).ready(function() {
 
-$("#randomNumber").html("53");
+  // Press Start Button
+  $("#startBtn").on("click", function() {
 
+    // Generate Random Number
+    var random = Math.floor(Math.random() * 100) +1;
+
+      // Display Random Number at id = randomNumber
+      $("#randomNumber").html(random);
+
+  });  // End of Random Number function
+
+  var totalScore = 0;
+
+  $("#opalBtn").on("click", function() {
+
+      var random1 = Math.floor(Math.random() * 10) +1;
+
+      totalScore += parseInt(random1);
+
+      $("#totalScore").html(totalScore);
+
+  });  // End OpalBtn
+
+    $("#amberBtn").on("click", function() {
+
+      var random1 = Math.floor(Math.random() * 10) +1;
+
+      totalScore += parseInt(random1);
+
+      $("#totalScore").html(totalScore);
+
+  });  // End amberBtn  
+
+    $("#zirconBtn").on("click", function() {
+
+      var random1 = Math.floor(Math.random() * 10) +1;
+
+      totalScore += parseInt(random1);
+
+      $("#totalScore").html(totalScore);
+
+  });  // End zirconBtn
+
+    $("#quartzBtn").on("click", function() {
+
+      var random1 = Math.floor(Math.random() * 10) +1;
+
+      totalScore += parseInt(random1);
+
+      $("#totalScore").html(totalScore);
+
+  });  // End quartzBtn
+
+    if (totalScore === randomNumber) {
+      wins++;
+      alert("You Win!!!");
+    }
+
+    else if (totalScore >= randomNumber) {
+      losses--
+      alert("You Lose!!!");
+    }
 
 });  // End of Document Ready
+
+
+
+
+
+/* Generate a random # by clicking button
+
+// in the body:  button id="random-button"
+
+<h1 class="text-center" id="random-number"></h1>
+
+$(document.ready(function() {
+  
+  $("random-button").on("click", function() {
+  
+    var random = Math.floor(Math.random() * 1000) + 1;
+
+    $("#random-number").html(random);
+
+  });
+
+})
+
+
+
+
+
+/* On Click Example:
+
+jQuery(function($) {
+  $("#click-me").on("click", function() {
+  
+    //...we trigger an alert:
+    alert("I've been clicked!");
+
+    });
+    $(document).on("keyup", function(event) {
+      console.log('keyup?', event.key);
+
+    });
+  });
+*/
 
 
 
